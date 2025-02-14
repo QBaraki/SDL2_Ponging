e0fb69aa7e37f0e50a8912d38bdbe708c01ec9ea
QBaraki <buithanhquang2006@gmail.com>
2025-02-14 15:07:10 +0700

    add: Geometry - 2D vector

---
+++ src/geometry/vec2d.cpp
@@ -0,0 +1,30 @@
+#include "vec2d.h"
+
+Vec2d::Vec2d() {
+  x = y = 0.0;
+}
+
+Vec2d::Vec2d(float x_, float y_) {
+  x = x_;
+  y = y_;
+}
+
+Vec2d Vec2d::operator=(const Vec2d& other) {
+  return Vec2d(x + other.x, y + other.y);
+}
+
+Vec2d& Vec2d::operator+=(const Vec2d& other) {
+  x += other.x;
+  y += other.y;
+  return *this;
+}
+
+Vec2d Vec2d::operator*(const Vec2d& other) {
+  return Vec2d(x * other.x, y * other.y);
+}
+
+Vec2d& Vec2d::operator*=(const Vec2d& other) {
+  x *= other.x;
+  y *= other.y;
+  return *this;
+}
---
+++ src/geometry/vec2d.h
@@ -0,0 +1,17 @@
+#ifndef VEC2D_H
+#define VEC2D
+
+class Vec2d {
+ public:
+  float x, y;
+
+  Vec2d();
+  Vec2d(float x_, float y_);
+
+  Vec2d operator=(const Vec2d& other);
+  Vec2d& operator+=(const Vec2d& other);
+  Vec2d operator*(const Vec2d& other);
+  Vec2d& operator*=(const Vec2d& other);
+};
+
+#endif  // VEC2D
